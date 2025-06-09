c161c7720c9465c57180338d09c13ec85ce008f0
OR-11 <93813653+OR-11@users.noreply.github.com>
2025-06-10 00:48:34 +0900

    メニューと注意事項を追加 いい感じに簡単にレイアウト

--- app/dining/cafeteria/page.jsx
+++ app/dining/cafeteria/page.jsx
@@ -1,3 +1,6 @@
+import TitleBarWithBack from "@/app/compoent/title_bar";
+import styles from "./page.module.css"
+
 export const metadata = {
   title: "蒼煌祭17th非公式ページ｜カフェテリア",
   description: "蒼煌祭17thのカフェテリアについての情報の非公式のページです。",
@@ -6,8 +9,38 @@ export const metadata = {
 export default function Cafeteria() {
   return (
     <div>
-      {/** カフェテリア */}
-      <p>カフェテリア</p>
+      <TitleBarWithBack backpage="/dining" pagename="カフェテリア"/>
+      <div>
+        <h1>メニュー</h1>
+        {/** メニューはデータベースから取り出してくる？ */}
+        {/** CSSいい感じにお願いします */}
+        <div className={styles.flex}>
+          <table className={styles.priceTable} style={{maxWidth: "100%"}}>
+            <tr>
+              <th scope="row" style={{ fontSize: "200%" }}>
+                ・カレー
+              </th>
+              <td style={{ fontSize: "200%" }}>
+                /400円
+              </td>
+            </tr>
+            <tr>
+              <th scope="row" style={{ fontSize: "200%" }}>
+                ・カツカレー
+              </th>
+              <td style={{ fontSize: "200%" }}>
+                /400円
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
+      <div>
+        <h1>注意事項</h1>
+        {/** 箇条書きのルールも必要？ */}
+        <p>・食券を買い、そのメニューの列に並んでください。途中、通路がありますので詰めすぎないようにお気を付けください。</p>
+        <p>・容器は使い捨てです。</p>
+      </div>
     </div>
   );
 }
