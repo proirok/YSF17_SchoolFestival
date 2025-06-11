b1becbf0e3ea04cb3cd0900b90e530208530ded1
OR-11 <93813653+OR-11@users.noreply.github.com>
2025-06-11 23:51:30 +0900

    食販団体一覧表示のテストデータの準備

--- app/dining/foodSales/page.jsx
+++ app/dining/foodSales/page.jsx
@@ -1,3 +1,5 @@
+import TitleBarWithBack from "@/app/compoent/title_bar";
+import Link from "next/link";
 export const metadata = {
   title: "蒼煌祭17th非公式ページ｜食販",
   description: "蒼煌祭17thの食販についての情報の非公式のページです。",
@@ -6,8 +8,26 @@ export const metadata = {
 export default function FoodSales() {
   return (
     <div>
-      {/** 食販 */}
-      <p>食販</p>
+      <TitleBarWithBack backpage="/dining" pagename="食品販売"/>
+      {/** こういう場合Linkとaどっちのほうがいいんだろ */}
+      {/** リンクが青くならない！なんでそうした！ */}
+      <p><Link href="#how-to-use-foodSales">食販の使い方</Link>も併せてお読みください</p>
+      <div style={{paddingBottom:"50rem"}}>
+        <h1>食販団体一覧</h1>
+        {/** 食販に限った企画で要求される機能が似ているのでprogram/slug/page.jsxの機能を流用しようかな？ */}
+        {/** ここを後で頑張る */}
+      </div>
+      <div id="how-to-use-foodSales">
+        <h1>食販の使い方</h1>
+        <iframe 
+          src="https://docs.google.com/presentation/d/e/2PACX-1vRSvoPrrU09BtDllWGcO3DX-EMw352OwDCt9hkw02RBTGMx-iumWjVnqMANDBV99leJqEyKKtKIeyIi/pubembed?start=false&loop=false&delayms=5000"
+          border={"none"}
+            width={320}
+            height={190}
+            allowFullScreen={true}
+            mozallowfullscreen={"true"}
+            webkitallowfullscreen={"true"}></iframe>
+      </div>
     </div>
   );
 }
