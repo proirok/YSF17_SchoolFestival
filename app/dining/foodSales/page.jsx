5a0d804b3620e2f309fea481f7b7a2e243a0b6c4
OR-11 <93813653+OR-11@users.noreply.github.com>
2025-06-14 14:59:32 +0900

    食販カード表示のスマホデザインだけ

--- app/dining/foodSales/page.jsx
+++ app/dining/foodSales/page.jsx
@@ -1,5 +1,6 @@
 import TitleBarWithBack from "@/app/compoent/title_bar";
 import Link from "next/link";
+import styles from "./page.module.css"
 export const metadata = {
   title: "蒼煌祭17th非公式ページ｜食販",
   description: "蒼煌祭17thの食販についての情報の非公式のページです。",
@@ -15,7 +16,62 @@ export default function FoodSales() {
       <div style={{paddingBottom:"50rem"}}>
         <h1>食販団体一覧</h1>
         {/** 食販に限った企画で要求される機能が似ているのでprogram/slug/page.jsxの機能を流用しようかな？ */}
-        {/** ここを後で頑張る */}
+        {/** スマホ想定の文字サイズ */}
+        <div id="foodSaleMenus">{/** 例。図形どうしよう */}
+          <div className={styles.foodSalesMenuCard}>{/** 一カード？の塊 */}
+            <table className={styles.foodSalesMenuBox}>
+              <tr>
+                <th scope="row" colSpan={2}>
+                  <p>ここには食販企画名が入るよ</p>
+                </th>
+              </tr>
+              <tr>
+                <th scope="row" className={styles.foodSalesMenuName}>
+                  <p>食べ物名1</p>
+                </th>
+                <td className={styles.foodSalesMenuName2Price}>
+                  <p>―</p>
+                </td>
+                <td className={styles.foodSalesMenuPrice}>
+                  <p>100円</p>
+                </td>
+              </tr>
+            </table>
+            <div className={styles.line}></div>
+            <details className={styles.ingredients}>
+              <summary>アレルゲン・原材料情報</summary>
+              <div className={styles.ingredientsTable}>
+              <table>
+                <tr>
+                  <th>
+                    <p>食べ物名1</p>
+                  </th>
+                </tr>
+                <tr>
+                  <table style={{marginLeft: "0.5em"}} className={styles.ingredient}>
+                    <tr>
+                      <th>
+                        <p>特定原材料27品目:</p>
+                      </th>
+                      <td>
+                        <p>卵、りんご</p>
+                     </td>
+                    </tr>
+                    <tr>
+                      <th>
+                        <p>原材料名: </p>
+                      </th>
+                      <td>
+                        <p>卵、米、りんご</p>
+                      </td>
+                    </tr>
+                  </table>
+                </tr>
+              </table>
+              </div>
+            </details>
+          </div>
+        </div>
       </div>
       <div id="how-to-use-foodSales">
         <h1>食販の使い方</h1>
