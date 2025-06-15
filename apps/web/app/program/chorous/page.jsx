c36634ac70369a25d2295553d6184677c6f2dc90
kanium3 <dev@kanium.me>
2025-06-15 20:45:14 +0900

    データ構造の変更に伴うページ内容の変更

--- apps/web/app/program/chorous/page.jsx
+++ apps/web/app/program/chorous/page.jsx
@@ -7,15 +7,15 @@ export default function Chorous() {
   return (
     <div>
       <h1>合唱祭</h1>
-      <div className={"time_place"}>
+      <div className="time_place">
         <h2>日時・場所</h2>
         <p>日時・場所</p>
       </div>
-      <div className={"slogan"}>
+      <div className="slogan">
         <h2>スローガン</h2>
         <p>スローガン・スローガンの意味</p>
       </div>
-      <div className={"songs"}>
+      <div className="songs">
         <h2>曲目</h2>
         <dl>
           <dt>1-1 曲名</dt>
