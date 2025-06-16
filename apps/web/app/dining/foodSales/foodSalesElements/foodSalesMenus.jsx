a3076694c7339fdc04ce577b546a8ed3dbb30674
OR-11 <93813653+OR-11@users.noreply.github.com>
2025-06-17 00:10:13 +0900

    json整形&出力関数を作成

---
+++ apps/web/app/dining/foodSales/foodSalesElements/foodSalesMenus.jsx
@@ -0,0 +1,14 @@
+import foodMenus from "../../../foodSales.mock.json" assert {type: "json"};
+import programs from "../../../program.mock.json" assert {type: "json"};
+
+export function foodSalesMenu() {
+  const fullFoodMenus = foodMenus.map(item => {
+    return {
+      team: programs.find(pItem => pItem.id == item.id).team,
+      name: programs.find(pItem => pItem.id == item.id).name,
+      menus: item.menus
+    }
+  });
+
+  return fullFoodMenus;
+}
