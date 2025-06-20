8306b51594048b2349e524402a09c87244da866e
ameirok <151885371+proirok@users.noreply.github.com>
2025-06-20 01:37:01 +0000

    usestateに値がうまく回らない現象発生。

--- apps/web/app/map/Map.jsx
+++ apps/web/app/map/Map.jsx
@@ -1,12 +1,29 @@
 "use client"
 import { CRS, LatLng, LatLngBounds, icon} from 'leaflet';
-import React from 'react';
-import { MapContainer, ImageOverlay, LayersControl, Marker, Popup} from 'react-leaflet'
+import React,{useState} from 'react';
+import { MapContainer, ImageOverlay, LayersControl, Marker, Popup, useMapEvents} from 'react-leaflet'
 import Link from 'next/link'
 import Image from 'next/image'
 import './Map.css';
 import 'leaflet/dist/leaflet.css';
+
+const {BaseLayer} = LayersControl;
+
+const layers = [{ name : "1階", url: "https://raw.githubusercontent.com/proirok/buffer/refs/heads/main/sample_map.svg"},
+  {name : "2階", url: "https://raw.githubusercontent.com/proirok/buffer/refs/heads/main/sample_map2.svg"}]
+
+function Control({setlayers}){
+  useMapEvents({layeradd(e){
+    if(e.layer.options && e.layer.options.name){
+      setlayers(e.layer.options.name)
+    }
+  }
+})
+}
+
 export default function Map(){
+  const [baselayer,setlayers] = useState(layers[0].name);
+
   const picwidth = 960;
   const picheight = 540;
   const position1 = [picheight/2,picwidth/2];
@@ -27,21 +44,23 @@ export default function Map(){
     maxBounds={[[0,0], [picheight, picwidth]]}
   >
     <LayersControl position="bottomright" collapsed="false">
-      <LayersControl.BaseLayer checked name = "1階">
-        <ImageOverlay url={'https://raw.githubusercontent.com/proirok/buffer/refs/heads/main/sample_map.svg'} bounds={new LatLngBounds([[0, 0], [picheight, picwidth]])} />
-      </LayersControl.BaseLayer>
-      <LayersControl.BaseLayer name = "2階">
-        <ImageOverlay url={'https://raw.githubusercontent.com/proirok/buffer/refs/heads/main/sample_map2.svg'} bounds={new LatLngBounds([[0, 0], [picheight, picwidth]])} />
-      </LayersControl.BaseLayer>
+      {layers.map(layer => (
+        <BaseLayer checked = {layer.name === layers[0].name} name={layer.name} key={layer.name}>
+          <ImageOverlay url={layer.url} bounds={new LatLngBounds([[0, 0], [picheight, picwidth]])} />
+        </BaseLayer>
+      ))}
+      {baselayer == "2階" && (
+        <Marker position = {position1} icon={ICON}>
+          <Popup>
+            <Image src="https://raw.githubusercontent.com/proirok/buffer/refs/heads/main/map_pin.svg" alt="マップピンアイコン画像" width={200} height={100}/>
+            <Link href="https://www.mrchildren.jp/" target="blank" rel="noopener noreferrer">
+              Mr.Childrenのホームページ
+            </Link>
+          </Popup>
+          </Marker>
+      )}
     </LayersControl>
-    <Marker position = {position1} icon={ICON}>
-      <Popup>
-        <Image src="https://raw.githubusercontent.com/proirok/buffer/refs/heads/main/map_pin.svg" alt="マップピンアイコン画像" width={200} height={100}/>
-        <Link href="https://www.mrchildren.jp/" target="blank" rel="noopener noreferrer">
-        Mr.Childrenのホームページ
-        </Link>
-      </Popup>
-    </Marker>
+    <Control setlayers={setlayers} />
 </MapContainer>
   )
 };
