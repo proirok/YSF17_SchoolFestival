77ee059ef0d1aabbd8f268321d1c2933f74320ad
ameirok <151885371+proirok@users.noreply.github.com>
2025-06-20 22:52:13 +0900

    技術的なコメントを追加
    アドバイスよろしくお願いします

--- apps/web/app/map/Map.jsx
+++ apps/web/app/map/Map.jsx
@@ -7,12 +7,12 @@ import Image from 'next/image'
 import './Map.css';
 import 'leaflet/dist/leaflet.css';  //リーフレットの本体のCSSの読み込み(これしないと地図が崩れる)
 
-const {BaseLayer} = LayersControl;  //
+const {BaseLayer} = LayersControl; //地図関連の値はLayersControlに入っているためそれを変数として定義
 
 const layers = [{ name : "1階", url: "https://raw.githubusercontent.com/proirok/buffer/refs/heads/main/sample_map.svg"},
   {name : "2階", url: "https://raw.githubusercontent.com/proirok/buffer/refs/heads/main/sample_map2.svg"}]
 
-function Control({setlayers}){
+function Control({setlayers}){  //地図レイヤーの名前をここで取得
   useMapEvents({layeradd(e){
     if(e.layer.options && e.layer.options.name){
       setlayers(e.layer.options.name)
@@ -44,7 +44,7 @@ export default function Map(){
     maxBounds={[[0,0], [picheight, picwidth]]}
   >
     <LayersControl position="bottomright" collapsed="false">
-      {layers.map(layer => (
+      {layers.map(layer => (  //※1と対応。※1をleafletで地図にしたものを配列化することでして地図として表示させる
         <BaseLayer checked = {layer.name === layers[0].name} name={layer.name} key={layer.name}>
           <ImageOverlay url={layer.url} bounds={new LatLngBounds([[0, 0], [picheight, picwidth]])} />
         </BaseLayer>
@@ -64,3 +64,11 @@ export default function Map(){
 </MapContainer>
   )
 };
+
+/*
+その他技術的なコメント
+layerをグループ化する方法は存在し、LayerGroupで普通にできる
+ひょっとするとLayerGroupをベースグループに使えれば普通にできるかも？
+ワンチャンかけて試してみます
+なんでベースレイヤーの変更だけ載っていないんですかねreactLeafletくん......
+*/
